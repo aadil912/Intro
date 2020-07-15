fe368843171f93cf3a3bf81f08a642a0f4221f16
aadil912 <aadilahnaf@gmai.com>
2020-07-15 12:14:34 +0600

    Final Commit

---
+++ base.py
@@ -0,0 +1,13 @@
+import random
+import sys
+
+name = input("Type your name. \n")
+print(f"Welcome {name}")
+age = input("Type your age. \n")
+if int(age) < 18:
+    print("You are too young to play this game. \n Thank you!")
+else:
+    print("Let's start the game")
+    char_type = input("Type the corresponding Character number \n 1. Mercenary \n 2. Wizard \n 3. King")
+    if 
+quit()
